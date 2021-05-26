e10a828062b2b086448f436171fb9f522b6a86f7
tumao <yan.wang@zilliz.com>
2021-05-26 16:34:15 +0800

    add single type page

--- config/database.js
+++ config/database.js
@@ -1,15 +1,50 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+module.exports = () => {
+  const url =
+    process.env.CUSTOMCONNSTR_pg ||
+    "postgresql://postgres:docker@127.0.0.1:5432/milvus-strapi";
+
+  const getDetailFromUrl = (url) => {
+    if (!url) return {};
+    url = url.replace("postgresql://", "");
+    const [username, pwdHost, portDatabase] = url.split(":");
+    const [password, host] = pwdHost.split("@");
+    const [port, database] = portDatabase.split("/");
+
+    return {
+      username,
+      password,
+      host,
+      port,
+      database,
+    };
+  };
+
+  const {
+    username = "",
+    password = "",
+    host = "",
+    port = "",
+    database = "",
+  } = getDetailFromUrl(url);
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: host || "127.0.0.1",
+          port: port || 5432,
+          database: database || "strapi",
+          username: username || "postgres",
+          password: password || "postgres",
+          ssl: process.env.NODE_ENV === "production",
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  };
+};
